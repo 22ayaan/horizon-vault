7728ae336ea07b2b3f6dbee9fb9581959609ca8d
Ayaan Siddiqui <ars394@pitt.edu>
2024-08-13 15:57:15 +0530

    implement recent transactions table.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
@@ -6,17 +7,16 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-
   const accounts = await getAccounts({
     userId: loggedIn?.$id,
   });
+
   if (!accounts) return;
 
   const accountsData = accounts?.data;
-
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-
   const account = await getAccount({ appwriteItemId });
 
   // console.log({
@@ -40,6 +40,13 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
+
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
       <RightSideBar
         user={loggedIn}
